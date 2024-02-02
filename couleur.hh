500e9f9687c79ce38483fdbf5b4fb4681940ed87
leg7 <dev@leonardgomez.xyz>
2024-02-02 15:53:58 +0100

    New: Add orange to couleur.hh

--- couleur.hh
+++ couleur.hh
@@ -11,6 +11,7 @@ class Couleur
                 Vert,
                 Bleu,
                 Jaune,
+                Orange,
                 Violet,
                 Magenta,
                 Cyan,
