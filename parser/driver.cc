acd1a3c7f50e106c67eeee53f5e1f9c046469e5c
leg7 <dev@leonardgomez.xyz>
2024-02-02 13:25:20 +0100

    New: Vecteurs de formes dans driver

--- parser/driver.cc
+++ parser/driver.cc
@@ -17,3 +17,43 @@ void Driver::setVariable(const std::string & name, double value) {
     //TODO Affecte une valeur à une variable avec l'utilisation du contexte variables
 }
 */
+
+void Driver::ajouterCarre(int x1, int y1, int taille)
+{
+        _carres.push_back(Carre(x1, y1, taille));
+}
+
+void Driver::ajouterCercle(int x1, int y1, int rayon)
+{
+        _cercles.push_back(Cercle(x1, y1, rayon));
+}
+void Driver::ajouterChemin(int x, int y)
+{
+        _chemins.push_back(Chemin(x, y));
+}
+void Driver::ajouterEllipse(int x1, int y1, int longueur, int hauteur)
+{
+        _ellipses.push_back(Ellipse(x1, y1, longueur, hauteur));
+}
+void Driver::ajouterLigne(int x1, int y1, int x2, int y2)
+{
+        _lignes.push_back(Ligne(x1, y1, x2, y2));
+}
+void Driver::ajouterRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+{
+        _rectangles.push_back(Rectangle(x1, y1, x2, y2, x3, y3, x4, y4));
+}
+void Driver::ajouterTexte(int x1, int y1, std::string const& texte, std::string const& police)
+{
+        _textes.push_back(Texte(x1, y1, texte, police));
+}
+void Driver::ajouterTriangle(int x1, int y1, int longueur, int hauteur)
+{
+        _triangles.push_back(Triangle(x1, y1, longueur, hauteur));
+}
+
+void Driver::cheminContinuer(int x1, int y1)
+{
+        _chemins.end()->ajoutePoint(x1, y1);
+}
+
--- parser/driver.hh
+++ parser/driver.hh
@@ -2,12 +2,22 @@
 #define DRIVER_H
 
 #include <string>
+#include <vector>
+#include "../formes/formes_inc.hh"
 #include "../expressions/contexte.hh"
-
 
 class Driver {
 private:
-    Contexte variables;
+    Contexte _variables;
+
+    std::vector<Carre> _carres;
+    std::vector<Cercle> _cercles;
+    std::vector<Chemin> _chemins;
+    std::vector<Ellipse> _ellipses;
+    std::vector<Ligne> _lignes;
+    std::vector<Rectangle> _rectangles;
+    std::vector<Texte> _textes;
+    std::vector<Triangle> _triangles;
 
 public:
     Driver();
@@ -18,6 +28,16 @@ public:
     double  getVariable(const std::string& name) const;
     void    setVariable(const std::string& name, double value);
   */
+    void ajouterCarre(int x1, int y1, int taille);
+    void ajouterCercle(int x1, int y1, int rayon);
+    void ajouterChemin(int x, int y);
+    void ajouterEllipse(int x1, int y1, int longueur, int hauteur);
+    void ajouterLigne(int x1, int y1, int x2, int y2);
+    void ajouterRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4);
+    void ajouterTexte(int x1, int y1, std::string const& texte, std::string const& police);
+    void ajouterTriangle(int x1, int y1, int longueur, int hauteur);
+
+    void cheminContinuer(int x1, int y1);
 };
 
 #endif
