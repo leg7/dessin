d6929b2b9acd682d00de6e0df80c90b36f020abc
leg7 <dev@leonardgomez.xyz>
2024-02-13 12:06:18 +0100

    Bug: Valeur du map contexte passe a un ptr Element

--- src/Contexte.h
+++ src/Contexte.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <map>
 #include <string>
+#include <memory>
 #include "elements/Element.h"
 
 class Contexte
 {
-	std::map<std::string, Element> data;
+	std::map<std::string, std::shared_ptr<Element>> data;
 
 	public:
 	bool add(const std::string &nom, const Element &e) noexcept;
