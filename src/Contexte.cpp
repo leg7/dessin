22f1cdf5d6b0f5a3519cc4773a2c3e3723a06f6a
leg7 <dev@leonardgomez.xyz>
2024-02-17 13:38:57 +0100

    New: Implement contexte

--- src/Contexte.cpp
+++ src/Contexte.cpp
@@ -2,10 +2,11 @@
 
 bool Contexte::add(const std::string &nom, const std::shared_ptr<Element> &e) noexcept
 {
-
+	const auto ret = _data.insert({nom, e});
+	return ret.second;
 }
 
 bool Contexte::rm(const std::string &nom) noexcept
 {
-
+	return _data.erase(nom);
 }
--- src/Contexte.h
+++ src/Contexte.h
@@ -6,7 +6,7 @@
 
 class Contexte
 {
-	std::map<std::string, std::shared_ptr<Element>> data;
+	std::map<std::string, std::shared_ptr<Element>> _data;
 
 	public:
 	bool add(const std::string &nom, const std::shared_ptr<Element> &e) noexcept;
